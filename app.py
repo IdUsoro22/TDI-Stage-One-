9b498f025b68c17eec53efd87fdf80115de1d923
omijieprecious <omijieprecious09@gmail.com>
2025-06-30 10:24:10 +0100

    Updated files with recent edits

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import streamlit as st
-from dashboard import run_dashboard
+# from dashboard import run_dashboard
 from prediction import run_prediction
 
 # Set page config (must be first Streamlit command)
-st.set_page_config(page_title="Loan Dashboard", page_icon="📊", layout="wide")
+st.set_page_config(page_title="Loan Prediction", page_icon="📊", layout="wide")
 
 # Load and display logo and title
 logo_path = "image\logo.png" 
 
 # Header layout
-col1, col2 = st.columns([1, 20])
+col1, col2 = st.columns([1, 10])
 with col1:
     st.image(logo_path, width=80)
 with col2:
@@ -23,9 +23,9 @@ with col2:
 st.markdown("---")
 
 # Navigation Sidebar
-page = st.sidebar.radio("Navigation", ["Dashboard", "Predict Loan Approval"])
+# page = st.sidebar.radio("Navigation", ["Dashboard", "Predict Loan Approval"])
 
-if page == "Dashboard":
-    run_dashboard()
-elif page == "Predict Loan Approval":
-    run_prediction()
+#if page == "Dashboard":
+ #   run_dashboard()
+# elif page == "Predict Loan Approval":
+run_prediction()
